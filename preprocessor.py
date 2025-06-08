cd484786341e9542a2450838664615d4edd52235
bokuldutta09 <154738052+bokuldutta09@users.noreply.github.com>
2025-06-09 00:19:32 +0530

    preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -2,7 +2,6 @@ import re
 import pandas as pd
 import unicodedata
 
-
 def clean_message(msg):
     if isinstance(msg, str):
         msg = unicodedata.normalize('NFKD', msg)
@@ -12,24 +11,37 @@ def clean_message(msg):
 
 
 def preprocess(data):
-    pattern = r'\[*(\d{2}/\d{2}/\d{2,4}), (\d{2}:\d{2}:\d{2})\]\s?(.*?): (.+)'
+    android_pattern = r'(\d{2}/\d{2}/\d{2,4}), (\d{2}:\d{2}) - (.*?): (.*)'
+    iphone_pattern = r'\[*(\d{2}/\d{2}/\d{2,4}), (\d{2}:\d{2}:\d{2})\]\s?(.*?): (.+)'
+
+    android_matches = re.findall(android_pattern, data)
+    iphone_matches = re.findall(iphone_pattern, data)
+
+    if iphone_matches:
+        format_type = 'iphone'
+        matches = iphone_matches
+    elif android_matches:
+        format_type = 'android'
+        matches = android_matches
+    else:
+        raise ValueError("Chat format not recognized")
 
     datetime_list = []
     name_message_list = []
 
 
-    matches = re.findall(pattern, data)
-    for date, time, name, message in matches:
+    for match in matches:
+        date, time, name, message = match
         datetime = f"{date} {time}"
         name_message = f"{name}: {message}"
 
         datetime_list.append(datetime)
         name_message_list.append(name_message)
 
-
-
     df = pd.DataFrame({'user_message': name_message_list, 'message_date': datetime_list})
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y %H:%M:%S')
+
+    date_format = '%d/%m/%y %H:%M:%S' if format_type == 'iphone' else '%d/%m/%y %H:%M'
+    df['message_date'] = pd.to_datetime(df['message_date'], format=date_format, errors='coerce')
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
     users = []
@@ -45,11 +57,8 @@ def preprocess(data):
 
     df['user'] = users
     df['message'] = messages
-
     df['message'] = df['message'].apply(clean_message)
-
     df.drop(columns=['user_message'], inplace=True)
-
     df['only_date'] = df['date'].dt.date
     df['year'] = df['date'].dt.year
     df['month_num'] = df['date'].dt.month
@@ -59,15 +68,16 @@ def preprocess(data):
     df['minute'] = df['date'].dt.minute
     df['day_name'] = df['date'].dt.day_name()
 
-    period = []
-    for hour in df[['day_name', 'hour']]['hour']:
-        if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
-        elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
-        else:
-            period.append(str(hour) + "-" + str(hour + 1))
 
+    period = []
+    for hour in df['hour']:
+        if hour == 23:
+            period.append("23-00")
+        elif hour == 0:
+            period.append("00-01")
+        else:
+            period.append(f"{hour:02d}-{(hour + 1) % 24:02d}")
     df['period'] = period
+
 
     return df
