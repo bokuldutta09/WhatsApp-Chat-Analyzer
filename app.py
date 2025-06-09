70f20329afbb8f3930710f4c70abcccb42c81664
bokuldutta09 <154738052+bokuldutta09@users.noreply.github.com>
2025-06-09 20:32:19 +0530

    app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from helper import most_common_words
+from datetime import timedelta
 
 st.set_page_config(
     page_title="Chat Analysis",
@@ -16,8 +17,15 @@ uploaded_file = st.sidebar.file_uploader("Choose a file", type=["txt"])
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
+    # st.text(data)
     df = preprocessor.preprocess(data)
 
+    min_date = df['only_date'].min()
+    max_date = df['only_date'].max()
+    date_range_days = (max_date - min_date).days + 1
+
+
+    # Remove group notification
     def is_group_notification(msg):
         patterns = [
             "added", "removed", "changed the subject to",
@@ -27,15 +35,22 @@ if uploaded_file is not None:
         ]
         return any(p in msg.lower() for p in patterns)
     df = df[df['user'].notnull() & ~df['message'].apply(is_group_notification)]
+    # st.dataframe(df)
 
+    # Fetch unique user
     user_list = df['user'].unique().tolist()
+    if 'messages and calls are end-to-end encrypted' in df.iloc[0]['message'].lower():
+        possible_group_name = df.iloc[0]['user']
+        if possible_group_name in user_list:
+            user_list.remove(possible_group_name)
+
     user_list.sort()
     user_list.insert(0, 'Overall')
     selected_user = st.sidebar.selectbox("Show analysis wrt", user_list)
 
-
     if st.sidebar.button("Show Analysis"):
 
+        # Stats Area
         num_messages, words, num_media_message, num_links = helper.fetch_stats(selected_user, df)
 
         st.title("Top Statistics")
@@ -54,46 +69,67 @@ if uploaded_file is not None:
             st.header("links Shared")
             st.title(num_links)
 
-        st.title("Monthly Timeline")
-        timeline = helper.monthly_timeline(selected_user, df)
-        fig,ax = plt.subplots()
-        ax.plot(timeline['time'], timeline['message'], color='green')
-        plt.xticks(rotation='vertical')
-        st.pyplot(fig)
-
-        st.title("Daily Timeline")
-        daily_timeline = helper.daily_timeline(selected_user, df)
-        fig, ax = plt.subplots()
-        ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
-        plt.xticks(rotation='vertical')
-        st.pyplot(fig)
-
-        st.title("Activity Map")
-        col1, col2 = st.columns(2)
-
-        with col1:
-            st.header("Most Busy Day")
-            busy_day  = helper.week_activity_map(selected_user, df)
+        # Monthly timeline
+        if date_range_days > 60:
+            st.title("Monthly Timeline")
+            timeline = helper.monthly_timeline(selected_user, df)
             fig, ax = plt.subplots()
-            ax.bar(busy_day.index, busy_day.values)
+            ax.plot(timeline['time'], timeline['message'], color='green')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
 
-        with col2:
-            st.header("Most Busy Month")
-            busy_month = helper.month_activity_map(selected_user, df)
+            st.title("Daily Timeline")
+            daily_timeline = helper.daily_timeline(selected_user, df)
             fig, ax = plt.subplots()
-            ax.bar(busy_month.index, busy_month.values, color = 'orange')
+            ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
 
-        st.title("Weekly activity map")
-        user_heatmap = helper.activity_heatmap(selected_user, df)
-        fig, ax = plt.subplots()
-        ax = sns.heatmap(user_heatmap)
-        st.pyplot(fig)
+        elif date_range_days > 7:
+            st.info("Short dataset detected (less than 2 months). Showing Daily Timeline only.")
+            st.title("Daily Timeline")
+            daily_timeline = helper.daily_timeline(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='blue')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+        else:
+            st.info("Very short dataset (≤ 1 week). Skipping timeline charts.")
 
+        # Activity week map
+        if date_range_days > 60:
+            st.title("Activity Map")
+            col1, col2 = st.columns(2)
 
+            with col1:
+                st.header("Most Busy Day")
+                busy_day  = helper.week_activity_map(selected_user, df)
+                fig, ax = plt.subplots()
+                ax.bar(busy_day.index, busy_day.values)
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+
+            with col2:
+                st.header("Most Busy Month")
+                busy_month = helper.month_activity_map(selected_user, df)
+                fig, ax = plt.subplots()
+                ax.bar(busy_month.index, busy_month.values, color = 'orange')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+        else:
+            st.warning("Very short dataset (≤ 1 week). Skipping Activity map charts.")
+
+        # Heatmap
+        if date_range_days > 7:
+            st.title("Weekly activity map")
+            user_heatmap = helper.activity_heatmap(selected_user, df)
+            fig, ax = plt.subplots()
+            ax = sns.heatmap(user_heatmap)
+            st.pyplot(fig)
+        else:
+            st.info("Not enough data for weekly heatmap (≤ 1 week).")
+
+        # Finding the busiest users in the group(Group level)
         if selected_user == 'Overall':
             st.title("Most Busy User")
 
@@ -108,22 +144,23 @@ if uploaded_file is not None:
             with col2:
                 st.dataframe(percent_df)
 
-
+        #WordCloud
         st.title("WordCloud")
         df_wc = helper.create_word_cloud(selected_user, df)
         fig, ax = plt.subplots()
         ax.imshow(df_wc)
+        ax.axis('off')
         st.pyplot(fig)
 
-
+        #Most Common words
         st.title("Most Common Words")
         most_common_df = helper.most_common_words(selected_user, df)
         fig, ax = plt.subplots()
-        ax.barh(most_common_df[0], most_common_df[1])
+        ax.barh(most_common_df['word'], most_common_df['count'], color='purple')
         plt.xticks(rotation='vertical')
         st.pyplot(fig)
 
-
+        # Emoji analysis
         st.title("Emoji Analysis")
         emoji_df = helper.emoji_helper(selected_user, df)
 
@@ -134,6 +171,6 @@ if uploaded_file is not None:
         with col2:
             st.header('Pie Chart')
             fig, ax = plt.subplots()
-            ax.pie(emoji_df[1].head(), labels=emoji_df[0].head(), autopct='%0.2f')
+            ax.pie(emoji_df['count'].head(), labels=emoji_df['emoji'].head(), autopct='%0.2f', shadow=False, startangle=90)
+            ax.axis('equal')
             st.pyplot(fig)
-
